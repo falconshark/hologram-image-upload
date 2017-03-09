dc68d2ae611291e88e58248cc27d6c9f8f0be988
Sardo <sardo@pixelactionstudio.com>
2017-03-09 12:47:47 +0800

    Improve style again

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,38 +3,40 @@ import {Modal} from 'react-bootstrap';
 import CropperCom from "./Cropper";
 
 class ModalCom extends React.Component {
-  constructor(props) {
-    super(props);
-    this.close = this.close.bind(this);
-    this.open = this.open.bind(this);
-    this.state = {
-      showModal: false
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.close = this.close.bind(this);
+        this.open = this.open.bind(this);
+        this.state = {
+            showModal: false
+        };
+    }
 
-  close() {
-    this.setState({showModal: false});
-  }
+    close() {
+        this.setState({showModal: false});
+    }
 
-  open() {
-    this.setState({showModal: true});
-  }
+    open() {
+        this.setState({showModal: true});
+    }
 
-  render(){
-    return (<div>
-      <img className="img-responsive img-thumbnail dropzone-preview" src={this.props.file.preview} onClick={this.open.bind(this)}/>
-      <Modal show={this.state.showModal} onHide={this.close.bind(this)}>
-        <Modal.Body>
-          <CropperCom
-            config={this.props.cropperConfig}
-            src={this.props.file.preview}
-            file={this.props.file}
-            onUpdate={this.props.cropperUpdate}
-            closeModal={this.close.bind(this)}/>
-        </Modal.Body>
-      </Modal>
-    </div>);
-  }
+    render(){
+        return (<div>
+            <div className="dropzone-image">
+                <img className="img-responsive img-thumbnail" src={this.props.file.preview} onClick={this.open.bind(this)}/>
+            </div>
+            <Modal show={this.state.showModal} onHide={this.close.bind(this)}>
+                <Modal.Body>
+                    <CropperCom
+                        config={this.props.cropperConfig}
+                        src={this.props.file.preview}
+                        file={this.props.file}
+                        onUpdate={this.props.cropperUpdate}
+                        closeModal={this.close.bind(this)}/>
+                </Modal.Body>
+            </Modal>
+        </div>);
+    }
 }
 
 export default ModalCom
