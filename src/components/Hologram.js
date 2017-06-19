e7faac8fdafdbc646dc47d630ae36917261f4ff5
Sardo <sardo@pixelactionstudio.com>
2017-06-19 18:17:56 +0800

    Start fix code style with ESLint

--- src/components/Hologram.js
+++ src/components/Hologram.js
@@ -1,191 +1,181 @@
 import React from 'react';
-import Dropzone from 'react-dropzone';
-import ModalCom from "./Modal";
 import request from 'superagent';
+import Dropzone from 'react-dropzone';
 import PropTypes from 'prop-types';
+import ModalCom from './Modal';
 
 class Hologram extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            files: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      files: [],
+    };
+  }
+  removeFile(file) {
+    const newList = this.state.files.filter((mFile) => file !== mFile)
+    this.setState({
+      files: newList,
+    });
+  }
+  // Convert Image to Data URL
+  convert(file){
+    return new Promise(function(resolve){
+      const image = new Image();
+      const canvas = document.createElement('canvas');
+      const canvasContext = canvas.getContext('2d');
+
+      image.src = file.preview;
+      image.crossOrigin = 'anonymous';
+      image.onload = function () {
+        canvas.width = image.width;
+        canvas.height = image.height;
+        canvasContext.drawImage(image, 0, 0, image.width, image.height);
+
+        const dataUrl = canvas.toDataURL();
+
+        const newFile = {
+          name: file.name,
+          key: file.key,
+          size: file.size,
+          preview: dataUrl,
+          type: file.type,
         };
-    }
+        resolve(newFile);
+      };
+    });
+  }
 
-    clear(){
-        this.setState({
-            files: []
+  //Upload file to upload hanlder by superagent
+
+  upload(file){
+    const convert = this.convert;
+    const uploader = this.props.uploader;
+    return new Promise(function(resolve, reject){
+      convert(file)
+      .then(file =>{
+        const data = JSON.stringify(file);
+        const req = request.post(uploader).send(data).end(function(err, res){
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(res);
         });
+      });
+    });
+  }
+
+  onOpenClick() {
+    this.dropzone.open();
+  }
+
+  onDrop(acceptedFiles) {
+    var files = this.state.files;
+
+    for(let acceptedFile of acceptedFiles){
+      let file = {
+        'name': acceptedFile['name'],
+        'key': Math.random().toString(36).substring(1),
+        'size': acceptedFile['size'],
+        'preview': acceptedFile['preview'],
+        'origin': acceptedFile['preview'],
+        'type': acceptedFile['type']
+      }
+
+      if(files.length < this.props.maxFiles){
+        files.push(file);
+      }else{
+        break;
+      }
     }
 
-    removeFile(file) {
-      const newList = this.state.files.filter((mFile) => file !== mFile)
-      this.setState({
-        files: newList
-      })
-    }
-    //Convert Image to Data URL
+    this.setState({
+      files: files
+    });
+  }
 
-    convert(file){
-        return new Promise(function(resolve){
-            var image = new Image();
-            var canvas = document.createElement('canvas');
-            var canvasContext = canvas.getContext('2d');
+  onUpdate(updateFile){
+    var files = this.state.files;
+    var fileIndex = files.findIndex((file => file.key === updateFile.key));
+    files[fileIndex] = updateFile;
+    this.setState({
+      files: files
+    });
+  }
 
-            image.src = file['preview'];
-            image.crossOrigin = 'anonymous';
-            image.onload = function () {
-                canvas.width = image.width;
-                canvas.height = image.height;
-                canvasContext.drawImage(image, 0, 0, image.width, image.height);
+  //Open dropzone without click dropzone
 
-                var dataUrl = canvas.toDataURL();
-                file['preview'] = dataUrl;
+  onOpenClick() {
+    this.dropzone.open();
+  }
 
-                var newFile = {
-                    'name': file['name'],
-                    'key': file['key'],
-                    'size': file['size'],
-                    'preview': file['preview'],
-                    'type': file['type']
-                }
-                resolve(newFile);
-            }
-        });
-    }
-
-    //Upload file to upload hanlder by superagent
-
-    upload(file){
-        var convert = this.convert;
-        var uploader = this.props.uploader;
-        return new Promise(function(resolve, reject){
-            convert(file)
-            .then(file =>{
-                var data = JSON.stringify(file);
-                var req = request.post(uploader).send(data).end(function(err, res){
-                    if(err){
-                        reject(err);
-                        return;
-                    }
-                    resolve(res);
-                });
-            });
-        });
-    }
-
-    //Upload converted file by JSON format to server
-
-    onUpload(){
-        var files = this.state.files;
-        var funList = [];
-        var uploadedFile = [];
-        for(let file of files){
-            funList.push(this.upload(file));
-            uploadedFile.push({'key': file['key'], 'name': file['name']});
-        }
-
-        Promise.all(funList)
-        .then(res => {
-            this.props.onComplete({'response': res, 'files': uploadedFile});
-        })
-        .catch(err => {
-            console.log(err);
-        });
-    }
-
-    onOpenClick() {
-        this.dropzone.open();
-    }
-
-    onDrop(acceptedFiles) {
-        var files = this.state.files;
-
-        for(let acceptedFile of acceptedFiles){
-            let file = {
-                'name': acceptedFile['name'],
-                'key': Math.random().toString(36).substring(1),
-                'size': acceptedFile['size'],
-                'preview': acceptedFile['preview'],
-                'origin': acceptedFile['preview'],
-                'type': acceptedFile['type']
-            }
-
-            if(files.length < this.props.maxFiles){
-                files.push(file);
-            }else{
-                break;
-            }
-        }
-
-        this.setState({
-            files: files
-        });
-    }
-
-    onUpdate(updateFile){
-        var files = this.state.files;
-        var fileIndex = files.findIndex((file => file.key === updateFile.key));
-        files[fileIndex] = updateFile;
-        this.setState({
-            files: files
-        });
-    }
-
-    //Open dropzone without click dropzone
-
-    onOpenClick() {
-        this.dropzone.open();
-    }
-
-    render() {
-        return (
-            <div className="dropzone">
-                <Dropzone {... this.props.dropzoneConfig} accept={'image/*'}
-                    ref={(node) => { this.dropzone = node; }} onDrop={this.onDrop.bind(this)}
-                    disableClick={true}>
-                    <div>Drop images here to upload them.</div>
-                    {this.state.files.length > 0 ?
-                        <div className="image-wrapper">
-                            <button className="hologram-btn" type="button" onClick={this.onOpenClick.bind(this)}>Open Dropzone</button>
-                            <button className="hologram-btn" type="button" onClick={this.onUpload.bind(this)}>Upload</button>
-                            <p className="help-block">Click Image to crop it.</p>
-                                {this.state.files.map((file) =>
-                                    <ModalCom
-                                        key={file.key}
-                                        file={file}
-                                        removeFile={this.removeFile.bind(this)}
-                                        cropperConfig={this.props.cropperConfig}
-                                        cropperUpdate={this.onUpdate.bind(this)}/>
-                                )}
-                    </div> : <button className="hologram-btn" type="button" onClick={this.onOpenClick.bind(this)}>Open Dropzone</button>
-                }
-            </Dropzone>
-        </div>
+  render() {
+    return (
+      <div className="dropzone">
+        <Dropzone {... this.props.dropzoneConfig} accept={'image/*'}
+          ref={(node) => { this.dropzone = node; }} onDrop={this.onDrop.bind(this)}
+          disableClick={true}>
+          <div>Drop images here to upload them.</div>
+          {this.state.files.length > 0 ?
+            <div className="image-wrapper">
+              <button className="hologram-btn" type="button" onClick={this.onOpenClick.bind(this)}>Open Dropzone</button>
+              <button className="hologram-btn" type="button" onClick={this.onUpload.bind(this)}>Upload</button>
+              <p className="help-block">Click Image to crop it.</p>
+              {this.state.files.map((file) =>
+                <ModalCom
+                  key={file.key}
+                  file={file}
+                  removeFile={this.removeFile.bind(this)}
+                  cropperConfig={this.props.cropperConfig}
+                  cropperUpdate={this.onUpdate.bind(this)}/>
+              )}
+            </div> : <button className="hologram-btn" type="button" onClick={this.onOpenClick.bind(this)}>Open Dropzone</button>
+          }
+        </Dropzone>
+      </div>
     );
-};
+  };
 
-static propTypes = {
-  cropperConfig: PropTypes.object,
-  dropzoneConfig: PropTypes.object,
-  maxFiles: PropTypes.number,
-  uploader: PropTypes.string.isRequired,
-  onComplete: PropTypes.func,
-};
+  static propTypes = {
+    cropperConfig: PropTypes.object,
+    dropzoneConfig: PropTypes.object,
+    maxFiles: PropTypes.number,
+    uploader: PropTypes.string.isRequired,
+    onComplete: PropTypes.func,
+  };
 
-static defaultProps = {
+  static defaultProps = {
     onComplete: () => {},
     maxFiles: 10,
-    dropzoneConfig : {
-        style : {
-            width:'100%',
-            padding: '2.5em 0',
-            background: 'rgba(0,0,0,0.5)',
-            textAlign: 'center',
-            color: '#fff'
-        }
+    dropzoneConfig: {
+      style: {
+        width: '100%',
+        padding: '2.5em 0',
+        background: 'rgba(0,0,0,0.5)',
+        textAlign: 'center',
+        color: '#fff',
+      },
+    },
+  }
+
+  // Upload converted file by JSON format to server
+  onUpload() {
+    const files = this.state.files;
+    const funList = [];
+    const uploadedFile = [];
+    for (let file of files){
+      funList.push(this.upload(file));
+      uploadedFile.push({'key': file['key'], 'name': file['name']});
     }
-}
+
+    Promise.all(funList)
+    .then(res => {
+      this.props.onComplete({'response': res, 'files': uploadedFile});
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
 }
 
-export default Hologram
+export default Hologram;
