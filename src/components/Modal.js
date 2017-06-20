d06a80f932471afbb2e5d882fc1f019bb608fc88
Sardo <sardo@pixelactionstudio.com>
2017-06-20 16:44:53 +0800

    Fix all of the ESLint error of Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,46 +1,69 @@
+/* eslint
+react/forbid-prop-types: 'warn',
+no-restricted-syntax: 'warn',
+jsx-a11y/no-noninteractive-element-interactions: 'off'
+*/
+
 import React from 'react';
 import Modal from 'react-awesome-modal';
-import CropperCom from "./Cropper";
+import PropTypes from 'prop-types';
 import FaTimesCircle from 'react-icons/lib/fa/times-circle';
+import CropperCom from './Cropper';
 
 class ModalCom extends React.Component {
-    constructor(props) {
-        super(props);
-        this.openModal = this.openModal.bind(this);
-        this.closeModal = this.closeModal.bind(this);
-        this.state = {
-            visible : false
-        };
-    }
+  static propTypes = {
+    file: PropTypes.object,
+    cropperConfig: PropTypes.object,
+    removeFile: PropTypes.func,
+    cropperUpdate: PropTypes.func,
+  };
 
-    openModal() {
-        this.setState({visible : true});
-    }
+  static defaultProps = {
+    file: {},
+    cropperConfig: {},
+    removeFile: () => {},
+    cropperUpdate: () => {},
+  };
 
-    closeModal() {
-        this.setState({visible : false});
-    }
+  constructor(props) {
+    super(props);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      visible: false,
+    };
+  }
 
-    handleClick() {
-      this.props.removeFile(this.props.file)
-    }
+  openModal() {
+    this.setState({ visible: true });
+  }
 
-    render(){
-        return (
-            <div className="dropzone-image">
-                <img src={this.props.file['preview']} onClick={this.openModal.bind(this)}/>
-                <FaTimesCircle className="remove-icon" onClick={() => this.handleClick()} />
-                <Modal visible={this.state.visible} onClickAway={() => this.closeModal()} width="400">
-                    <CropperCom
-                        config={this.props.cropperConfig}
-                        src={this.props.file['origin']}
-                        file={this.props.file}
-                        onUpdate={this.props.cropperUpdate}
-                        closeModal={this.closeModal.bind(this)}/>
-                </Modal>
-            </div>
-        );
-    }
+  closeModal() {
+    this.setState({ visible: false });
+  }
+
+  handleClick() {
+    this.props.removeFile(this.props.file);
+  }
+
+  render() {
+    return (
+      <div className="dropzone-image">
+        <img alt="file preview" src={this.props.file.preview} onClick={this.openModal} />
+        <FaTimesCircle className="remove-icon" onClick={() => this.handleClick()} />
+        <Modal visible={this.state.visible} onClickAway={() => this.closeModal()} width="400">
+          <CropperCom
+            config={this.props.cropperConfig}
+            src={this.props.file.origin}
+            file={this.props.file}
+            onUpdate={this.props.cropperUpdate}
+            closeModal={this.closeModal}
+          />
+        </Modal>
+      </div>
+    );
+  }
 }
 
-export default ModalCom
+export default ModalCom;
