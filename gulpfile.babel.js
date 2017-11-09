4024411d5c606076def2b5a43882228641023a44
Sardo <sardo@pixelactionstudio.com>
2017-11-09 16:28:22 +0800

    Fix gulp file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,12 +8,14 @@ var webpackCfg = require('./webpack.config.js');
 
 gulp.task('watch', function () {
   gulp.watch('src/**/*.js', ['build:js']);
-  gulp.watch('../src/css/*.scss', ['build:scss']);
+  gulp.watch('./src/css/*.scss', ['build:scss']);
+  gulp.watch('./src/css/*.css', ['build:css']);
 });
 
 gulp.task('build:scss', function() {
-  return gulp.src('./src/css/*.scss')
+  return gulp.src('./src/css/Hologram.scss')
   .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./src/css/'));
 });
 
 gulp.task('build:css', function(){
